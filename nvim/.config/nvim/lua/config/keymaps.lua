52722e9d75a659462f2e09151a82bedade83e90f
Jaime Florian <jaimeflorian27@gmail.com>
2024-11-27 23:55:24 -0800

    feat: nvim: remapped Ctrl+C to act as Esc.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,7 @@ keymap.set("n", "<leader>t", ":NvimTreeFindFileToggle<CR>", {noremap = true, sil
 keymap.set("n", "<leader>t", ":NvimTreeFindFileToggle<CR>", {noremap = true, silent = true})
 keymap.set('n', '<Tab>', ':bn<CR>');
 keymap.set('n', '<S-Tab>', ':bp<CR>');
+
+-- Remap Ctrl+C to Esc. LSP diagnostics don't show with regular Ctrl+C behavior.
+keymap.set({'i', 'n', 'v'}, '<C-c>', '<Esc>', { silent = true, noremap = true })
+
